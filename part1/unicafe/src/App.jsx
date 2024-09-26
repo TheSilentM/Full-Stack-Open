303e1ba6c530538ad7c67f1bc763d014149f4727
TheSilentM <137522609+TheSilentM@users.noreply.github.com>
2024-09-26 18:46:48 +0200

    unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,6 +2,21 @@
 //useState for tracking feedback of every person for every button
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  return (
+    <>
+    <h1>{props.title}</h1>
+    <br />
+      <h4>Good: {props.good}</h4>
+      <h4>Neutral: {props.neutral}</h4>
+      <h4>Bad: {props.bad}</h4>
+      <h4>All: {props.total}</h4>
+      <br />
+      <h4>Average: {props.average}</h4>
+      <h4>Positive percentage: {props.percentage}</h4>
+    </>
+  )
+}
 
 const App = () => {
 
@@ -9,19 +24,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const goodFeedback = () => {
+    setGood(good + 1);
+  }
+
+  const neutralFeedback = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const badFeedback = () => {
+    setBad(bad + 1);
+  }
+
+  const total = good + neutral + bad;
+  const average = (good * 1) + (neutral * 0) + (bad * -1) / total;
+  const percentage = (((good * 1)) / total) * 100 + " %";
+ 
   return (
     <>
       <h1>Give Feedback</h1>
       <br />
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <button onClick={goodFeedback}>Good</button>
+      <button onClick={neutralFeedback}>Neutral</button>
+      <button onClick={badFeedback}>Bad</button>
       <br />
-      <h1>Statistics</h1>
-      <br />
-      <h4>Good: {good}</h4>
-      <h4>Neutral: {neutral}</h4>
-      <h4>Bad: {bad}</h4>
+      <Statistics title="Statistics" good={good} neutral={neutral} bad={bad} average={average} percentage={percentage} total={total}/>
     </>
   )
 }
