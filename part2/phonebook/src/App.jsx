5128ab1af0f3b81ba7b349d1de9381da2ead9d6e
TheSilentM <137522609+TheSilentM@users.noreply.github.com>
2024-10-10 16:50:05 +0200

    phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,28 +3,31 @@ import PeopleList from "../components/PeopleList";
 import PersonForm from "../components/PersonForm";
 import SearchNumbers from '../components/SearchNumbers';
 import services from "../src/services/numbers";
-import axios from "axios";
+import "./index.css";
 
 const App = () => {
-  const [people, setPeople] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [people, setPeople] = useState(null);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchFilter, setSearchFilter] = useState("");
+  const [message, setMessage] = useState("");
 
   //promise asynchronous function
   const hook = () => {
     services.getAll().then(response => {
       setPeople(response.data);
-    });
+    }).catch(error => {
+      setMessage(`Information of ${newName} has already been removed from server`);
+      setTimeout(() => setMessage(null), 5000);
+    })
   }
 
   useEffect(hook, []);
+
+  if(!people){
+    return null;
+  }
 
   const addPerson = (e, id) => {
     e.preventDefault();
@@ -35,7 +38,7 @@ const App = () => {
     //.find creates a new array where you can take the id that corresponds to the selected person.
     //confirm with window.confirm and if is true, put the new number on the nameObject in the JSON server, using axios.put
     //if th id of the person is not equal to the id of the existingPerson, set people array to all the people array, else on the data that the response has send.
-    
+
     const existingPerson = people.find(person => person.name === newName);
     if (existingPerson) {
       const confirmUpdate = window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`);
@@ -49,6 +52,8 @@ const App = () => {
     } else {
       services.save(nameObject).then(response => {
         setPeople(people.concat(response.data));
+        setMessage(`${newName} added!`);
+        setTimeout(() => setMessage(null), 5000);
         setNewName("");
         setNewNumber("");
       })
@@ -78,13 +83,24 @@ const App = () => {
     }
   }
 
+  const Message = ({message}) => {
+    if (message === null) {
+      return null;
+    }
 
+    return (
+      <div className='notification'>
+        <p>{message}</p>
+      </div>
+    )
+  }
 
   return (
     <div>
       <div>
         <h1>Phonebook</h1>
       </div>
+      <Message message={message}/>
       <SearchNumbers people={people} searchFilter={searchFilter} handleSearchNumbers={handleSearchNumbers}/>
       <PersonForm add={addPerson} name={newName} handleChangePerson={handleChangePerson} number={newNumber} handleChangeNumber={handleChangeNumber}/>
       <h2>Numbers</h2>
