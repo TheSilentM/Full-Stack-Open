fbeff3d37a939b0cda3b4f5d2f1f4c0b11e88b15
TheSilentM <137522609+TheSilentM@users.noreply.github.com>
2024-10-12 17:19:16 +0200

    src

--- part2/data_for_countries/src/App.jsx
+++ part2/data_for_countries/src/App.jsx
@@ -23,7 +23,9 @@ const App = () => {
             <h2>{country.name}</h2>
             <p>Capital: {country.capital}</p>
             <p>Area: {country.area}</p>
+            <br />
             <p>Languages: {country.languages}</p>
+            <img src={country.flag} width="200px" height="150px" alt="Country flag"></img>
           </div>
         );
     } else {
